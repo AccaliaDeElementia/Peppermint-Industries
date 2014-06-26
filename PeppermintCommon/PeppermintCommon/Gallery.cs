1eba4444a7576704f528faf70613737613812b8d
Accalia de Elementia <accalia.de.elementia@live.com>
2014-06-26 18:27:21 -0400

    Initial project commit

---
+++ PeppermintCommon/PeppermintCommon/Gallery.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.AccessCache;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+
+namespace PeppermintCommon
+{
+    public class Gallery
+    {
+        private IEnumerable<StorageFile> Files;
+        private StorageFile Current;
+        public async Task<StorageFile> OpenFolder()
+        {
+            var picker = new FolderPicker();
+            picker.CommitButtonText = "Open Folder";
+            picker.ViewMode = PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation = PickerLocationId.Downloads;
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+            picker.FileTypeFilter.Add(".gif");
+            var folder = await picker.PickSingleFolderAsync();
+            StorageApplicationPermissions.FutureAccessList.AddOrReplace("GalleryFolder", folder);
+            var types = new[] { ".jpg", ".jpeg", ".png", ".gif", ".tif" };
+            Files = from file in await folder.GetFilesAsync()
+                        where types.Contains(file.FileType)
+                        orderby MungeName(file.Name)
+                        select file;
+            return Current = Files.FirstOrDefault();
+        }
+
+        public StorageFile NextImage()
+        {
+            if (Files == null || !Files.Any() || Current == null) return null;
+            var mname = MungeName(Current.Name);
+            // ReSharper disable once StringCompareIsCultureSpecific.1
+            return Current = Files.FirstOrDefault(file => string.Compare(MungeName(file.Name), mname) > 0);
+        }
+
+        private static readonly Regex MungePattern = new Regex(@"(\d+)|(\D+)");
+
+        private static string MungeName(string name)
+        {
+            return string.Join("",
+                from Match part in MungePattern.Matches(name)
+                select part.Groups[1].Length>0?part.Value.PadLeft(20, '0'):part.Value);
+        }
+    }
+}
---
+++ PeppermintCommon/PeppermintCommon/Image.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace PeppermintCommon
+{
+    public sealed partial class Image
+    {
+        public Image()
+        {
+            InitializeComponent();
+        }
+
+        public event EventHandler ImageLoaded = (o, e) => { };
+
+        public uint FrameCount { get; private set; }
+        private readonly List<WriteableBitmap> _frames = new List<WriteableBitmap>();
+
+        public async void LoadImage(IRandomAccessStreamReference file)
+        {
+            await DecodeImage(file);
+            BuildStoryBoard();
+            PlaybackStoryboard.Begin();
+
+            ImageLoaded(this, EventArgs.Empty);
+        }
+
+        private async Task DecodeImage(IRandomAccessStreamReference file)
+        {   
+            _frames.Clear();
+            using (var data = await file.OpenReadAsync())
+            {
+                var decoder = await BitmapDecoder.CreateAsync(data);
+
+                FrameCount = decoder.FrameCount;
+                for (var i = 0u; i < FrameCount; i += 1)
+                {
+                    var frame = await decoder.GetFrameAsync(i);
+                    var target = new WriteableBitmap(
+                        (int)decoder.OrientedPixelWidth, 
+                        (int)decoder.OrientedPixelHeight);
+                    
+                    var bitmapTransform = new BitmapTransform {InterpolationMode = BitmapInterpolationMode.Cubic};
+                    var pixelProvider = await frame.GetPixelDataAsync(
+                        BitmapPixelFormat.Bgra8, BitmapAlphaMode.Straight, 
+                        bitmapTransform, ExifOrientationMode.RespectExifOrientation,
+                        ColorManagementMode.ColorManageToSRgb);
+                    var pixels = pixelProvider.DetachPixelData();
+                    
+                    target.PixelBuffer.AsStream().Write(pixels, 0, pixels.Length);
+                    _frames.Add(target);
+                }
+            }
+        }
+        private void BuildStoryBoard()
+        {
+            PlaybackStoryboard.Stop();
+            PlaybackStoryboard.Children.Clear();
+            var anim = new ObjectAnimationUsingKeyFrames
+            {
+                BeginTime = TimeSpan.FromSeconds(0)
+            };
+
+            var ts = new TimeSpan();
+            var speed = TimeSpan.FromMilliseconds(100); // Standard GIF framerate 10 fps?
+
+            foreach (var frame in _frames)
+            {
+                var keyFrame = new DiscreteObjectKeyFrame
+                {
+                    KeyTime = KeyTime.FromTimeSpan(ts),
+                    Value = frame
+                };
+
+                ts = ts.Add(speed);
+                anim.KeyFrames.Add(keyFrame);
+            }
+
+            Storyboard.SetTarget(anim, TargetImage);
+            Storyboard.SetTargetProperty(anim, "Source");
+
+            PlaybackStoryboard.Children.Add(anim);
+        }
+    }
+}
