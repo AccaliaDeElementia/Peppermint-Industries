3e34396c23f4a87002dcbf99f7fdce9162591ac7
Accalia de Elementia <accalia.de.elementia@live.com>
2014-06-26 18:32:46 -0400

    adding missed file

---
+++ ImageGallery/ImageGallery/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using PeppermintCommon;
+
+namespace ImageGallery
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private readonly Gallery _imagegallery = new Gallery();
+
+        private async void OpenFolder_Click(object sender, RoutedEventArgs e)
+        {
+            var img = await _imagegallery.OpenFolder();
+            if (img == null) return;
+            Image.LoadImage(img);
+        }
+
+        private void NextImage_OnClick(object sender, RoutedEventArgs e)
+        {
+            var img = _imagegallery.NextImage();
+            if (img == null) return;
+            Image.LoadImage(img);
+        }
+    }
+}
